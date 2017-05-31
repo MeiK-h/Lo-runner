42823f8de473e49631937ebc7706ca2c42e41a96
MeiK <meik2333@gmail.com>
2017-05-31 22:02:09 +0800

    统一代码格式，添加注释

--- lorun/cext/lorun.c
+++ lorun/cext/lorun.c
@@ -21,61 +21,84 @@
 #include "run.h"
 #include "diff.h"
 
-
+/* 将Python传递的参数解析 */
 int initRun(struct Runobj *runobj, PyObject *args)
 {
+    /*
+    {
+        "args": ["./m"],                  #运行程序命令
+        "fd_in": fin.fileno(),            #输入文件描述符
+        "fd_out": fout.fileno(),          #输出文件描述符
+        "timelimit": 1000,                #时间限制(毫秒)
+        "memorylimit": 20000,             #内存限制(KB)
+        "runner": ,                       #运行用户
+        "trace": True/False,              #是否开启跟踪模式
+        "calls": range(0, 400),           #列表形式， 可以调用的名单
+        "files": {"/etc/ld.so.cache": 1}, #
+    }
+    */
     PyObject *config, *args_obj, *trace_obj, *time_obj, *memory_obj;
     PyObject *calls_obj, *runner_obj, *fd_obj;
-    
+
     if (!PyArg_ParseTuple(args, "O", &config))
         RAISE1("initRun parseTuple failure");
-    if(!PyDict_Check(config)) RAISE1("argument must be a dict");
-    
-    if((args_obj = PyDict_GetItemString(config, "args")) == NULL)
+    if (!PyDict_Check(config))
+        RAISE1("argument must be a dict");
+
+    if ((args_obj = PyDict_GetItemString(config, "args")) == NULL)
         RAISE1("must supply args");
-    
-    if((runobj->args = genRunArgs(args_obj)) == NULL) return -1;
-    
-    if((fd_obj = PyDict_GetItemString(config, "fd_in")) == NULL)
+
+    if ((runobj->args = genRunArgs(args_obj)) == NULL)
+        return -1;
+
+    if ((fd_obj = PyDict_GetItemString(config, "fd_in")) == NULL)
         runobj->fd_in = -1;
-    else runobj->fd_in = PyLong_AsLong(fd_obj);
-    if((fd_obj = PyDict_GetItemString(config, "fd_out")) == NULL)
+    else
+        runobj->fd_in = PyLong_AsLong(fd_obj);
+    if ((fd_obj = PyDict_GetItemString(config, "fd_out")) == NULL)
         runobj->fd_out = -1;
-    else runobj->fd_out = PyLong_AsLong(fd_obj);
-    if((fd_obj = PyDict_GetItemString(config, "fd_err")) == NULL)
+    else
+        runobj->fd_out = PyLong_AsLong(fd_obj);
+    if ((fd_obj = PyDict_GetItemString(config, "fd_err")) == NULL)
         runobj->fd_err = -1;
-    else runobj->fd_err = PyLong_AsLong(fd_obj);
-      
-    if((time_obj = PyDict_GetItemString(config, "timelimit")) == NULL)
+    else
+        runobj->fd_err = PyLong_AsLong(fd_obj);
+
+    if ((time_obj = PyDict_GetItemString(config, "timelimit")) == NULL)
         RAISE1("must supply timelimit");
     runobj->time_limit = PyLong_AsLong(time_obj);
-    
-    if((memory_obj = PyDict_GetItemString(config, "memorylimit")) == NULL)
+
+    if ((memory_obj = PyDict_GetItemString(config, "memorylimit")) == NULL)
         RAISE1("must supply memorylimit");
     runobj->memory_limit = PyLong_AsLong(memory_obj);
-    
-    if((runner_obj = PyDict_GetItemString(config, "runner")) == NULL)
+
+    if ((runner_obj = PyDict_GetItemString(config, "runner")) == NULL)
         runobj->runner = -1;
     else
         runobj->runner = PyLong_AsLong(runner_obj);
-    
-    if((trace_obj = PyDict_GetItemString(config, "trace")) != NULL){
-        if(trace_obj == Py_True){
+
+    if ((trace_obj = PyDict_GetItemString(config, "trace")) != NULL) {
+        if (trace_obj == Py_True) {
             runobj->trace = 1;
             //trace mode: supply calls and files to access.
-            if((calls_obj = PyDict_GetItemString(config, "calls")) == NULL)
+            if ((calls_obj = PyDict_GetItemString(config, "calls")) == NULL)
                 RAISE1("trace == True, so you must specify calls.");
-            if(!PyList_Check(calls_obj))
+            if (!PyList_Check(calls_obj))
                 RAISE1("calls must be a list.");
-            if(initCalls(calls_obj, runobj->inttable)) return -1;
-            
-            if((runobj->files = PyDict_GetItemString(config, "files")) == NULL)
+            if (initCalls(calls_obj, runobj->inttable))
+                return -1;
+
+            if ((runobj->files = PyDict_GetItemString(config, "files")) == NULL)
                 RAISE1("trace == True, so you must specify files.");
-            if(!PyDict_Check(runobj->files))
+            if (!PyDict_Check(runobj->files))
                 RAISE1("files must be a dcit.");
-        } else runobj->trace = 0;
-    } else runobj->trace = 0;
-    
+        }
+        else
+            runobj->trace = 0;
+    }
+    else
+        runobj->trace = 0;
+
     return 0;
 }
 
@@ -84,31 +107,31 @@ PyObject *run(PyObject *self, PyObject *args)
     struct Runobj runobj = {0};
     struct Result rst = {0};
     rst.re_call = -1;
-    
-    if(initRun(&runobj, args)){
-        if(runobj.args)
+
+    if (initRun(&runobj, args)) {
+        if (runobj.args)
             free((void*)runobj.args);
         return NULL;
     }
-    
-    if(runit(&runobj, &rst) == -1)
+
+    if (runit(&runobj, &rst) == -1)
         return NULL;
-    if(runobj.args)
+    if (runobj.args)
         free((void*)runobj.args);
-    
+
     return genResult(&rst);
 }
 
 PyObject* check(PyObject *self, PyObject *args)
 {
     int user_fd, right_fd, rst;
-    
+
     if (!PyArg_ParseTuple(args, "ii", &right_fd, &user_fd))
         RAISE0("run parseTuple failure");
-    
-    if(checkDiff(right_fd, user_fd, &rst) == -1)
+
+    if (checkDiff(right_fd, user_fd, &rst) == -1)
         return NULL;
-    
+
     return Py_BuildValue("i", rst);
 }
 
@@ -148,15 +171,15 @@ static int lorun_ext_clear(PyObject *m) {
 }
 
 static struct PyModuleDef moduledef = {
-        PyModuleDef_HEAD_INIT,
-        "_lorun_ext",
-        NULL,
-        sizeof(struct module_state),
-        lorun_methods,
-        NULL,
-        lorun_ext_traverse,
-        lorun_ext_clear,
-        NULL
+    PyModuleDef_HEAD_INIT,
+    "_lorun_ext",
+    NULL,
+    sizeof(struct module_state),
+    lorun_methods,
+    NULL,
+    lorun_ext_traverse,
+    lorun_ext_clear,
+    NULL
 };
 
 PyObject *PyInit__lorun_ext(void) {
--- lorun/cext/run.c
+++ lorun/cext/run.c
@@ -30,6 +30,7 @@
 const char *last_run_err;
 #define RAISE_RUN(err) {last_run_err = err;return -1;}
 
+/* 监控系统调用运行子进程 */
 int traceLoop(struct Runobj *runobj, struct Result *rst, pid_t pid) {
     int status, incall = 0;
     struct rusage ru;
@@ -39,16 +40,18 @@ int traceLoop(struct Runobj *runobj, struct Result *rst, pid_t pid) {
         if (wait4(pid, &status, WSTOPPED, &ru) == -1)
             RAISE_RUN("wait4 [WSTOPPED] failure");
 
+        /* 检查是否停止 */
         if (WIFEXITED(status))
             break;
         else if (WSTOPSIG(status) != SIGTRAP) {
+            /* 非内核产生的调停 */
             ptrace(PTRACE_KILL, pid, NULL, NULL);
             waitpid(pid, NULL, 0);
 
             rst->time_used = ru.ru_utime.tv_sec * 1000
-                    + ru.ru_utime.tv_usec / 1000
-                    + ru.ru_stime.tv_sec * 1000
-                    + ru.ru_stime.tv_usec / 1000;
+                             + ru.ru_utime.tv_usec / 1000
+                             + ru.ru_stime.tv_sec * 1000
+                             + ru.ru_stime.tv_usec / 1000;
             rst->memory_used = ru.ru_maxrss;
 
             switch (WSTOPSIG(status)) {
@@ -68,14 +71,10 @@ int traceLoop(struct Runobj *runobj, struct Result *rst, pid_t pid) {
             }
 
             rst->re_signum = WSTOPSIG(status);
-            rst->time_used = ru.ru_utime.tv_sec * 1000
-                    + ru.ru_utime.tv_usec / 1000
-                    + ru.ru_stime.tv_sec * 1000
-                    + ru.ru_stime.tv_usec / 1000;
-            rst->memory_used = ru.ru_maxrss;
             return 0;
         }
 
+        /* 复制跟踪器信息 */
         if (ptrace(PTRACE_GETREGS, pid, NULL, &regs) == -1)
             RAISE_RUN("PTRACE_GETREGS failure");
 
@@ -95,24 +94,27 @@ int traceLoop(struct Runobj *runobj, struct Result *rst, pid_t pid) {
                 rst->judge_result = RE;
                 if (ret == ACCESS_CALL_ERR) {
                     rst->re_call = REG_SYS_CALL(&regs);
-                } else {
+                }
+                else {
                     rst->re_file = lastFileAccess();
                     rst->re_file_flag = REG_ARG_2(&regs);
                 }
                 return 0;
             }
             incall = 0;
-        } else
+        }
+        else
             incall = 1;
 
+        /* 重新启动跟踪 */
         ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
     }
     
     
     rst->time_used = ru.ru_utime.tv_sec * 1000
-            + ru.ru_utime.tv_usec / 1000
-            + ru.ru_stime.tv_sec * 1000
-            + ru.ru_stime.tv_usec / 1000;
+                     + ru.ru_utime.tv_usec / 1000
+                     + ru.ru_stime.tv_sec * 1000
+                     + ru.ru_stime.tv_usec / 1000;
     rst->memory_used = ru.ru_maxrss;
 
 
@@ -126,20 +128,25 @@ int traceLoop(struct Runobj *runobj, struct Result *rst, pid_t pid) {
     return 0;
 }
 
+/* 不监控系统调用 */
 int waitExit(struct Runobj *runobj, struct Result *rst, pid_t pid) {
     int status;
     struct rusage ru;
 
+    /* 等待子进程结束 */
     if (wait4(pid, &status, 0, &ru) == -1)
         RAISE_RUN("wait4 failure");
 
+    /* 获得子进程的资源占用 */
     rst->time_used = ru.ru_utime.tv_sec * 1000
-            + ru.ru_utime.tv_usec / 1000
-            + ru.ru_stime.tv_sec * 1000
-            + ru.ru_stime.tv_usec / 1000;
+                     + ru.ru_utime.tv_usec / 1000
+                     + ru.ru_stime.tv_sec * 1000
+                     + ru.ru_stime.tv_usec / 1000;
     rst->memory_used = ru.ru_maxrss;
 
+    /* 判断是否为异常退出 */
     if (WIFSIGNALED(status)) {
+        /* 获得退出原因 */
         switch (WTERMSIG(status)) {
             case SIGSEGV:
                 if (rst->memory_used > runobj->memory_limit)
@@ -156,7 +163,9 @@ int waitExit(struct Runobj *runobj, struct Result *rst, pid_t pid) {
                 break;
         }
         rst->re_signum = WTERMSIG(status);
-    } else {
+    }
+    else {
+        /* 判断是否超过限制，此处的AC并不代表正确，只是说明没有异常错误 */
         if (rst->time_used > runobj->time_limit)
             rst->judge_result = TLE;
         else if (rst->memory_used > runobj->memory_limit)
@@ -190,6 +199,7 @@ int runit(struct Runobj *runobj, struct Result *rst) {
             _exit(r);\
         }
 
+        /* 重定向输入输出和错误流 */
         if (runobj->fd_in != -1)
             if (dup2(runobj->fd_in, 0) == -1)
                 RAISE_EXIT("dup2 stdin failure!")
@@ -202,21 +212,26 @@ int runit(struct Runobj *runobj, struct Result *rst) {
             if (dup2(runobj->fd_err, 2) == -1)
                 RAISE_EXIT("dup2 stderr failure")
 
+        /* 为进程设置限制 */
         if (setResLimit(runobj) == -1)
             RAISE_EXIT(last_limit_err)
 
+        /* 修改运行用户(如果提供了此参数的话)，防止恶意代码或者自行修改限制 */
         if (runobj->runner != -1)
             if (setuid(runobj->runner))
                 RAISE_EXIT("setuid failure")
 
+        /* 监控系统调用(如果开启了的话)，防止恶意代码 */
         if (runobj->trace)
             if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1)
                 RAISE_EXIT("TRACEME failure")
 
+        /* 开始执行 */
         execvp(runobj->args[0], (char * const *) runobj->args);
 
         RAISE_EXIT("execvp failure")
-    } else {
+    }
+    else {
         int r;
         char errbuffer[100] = { 0 };
 
@@ -229,6 +244,7 @@ int runit(struct Runobj *runobj, struct Result *rst) {
             return -1;
         }
 
+        /* 根据是否提供trace来决定使用哪种运行方式 */
         if (runobj->trace)
             r = traceLoop(runobj, rst, pid);
         else
